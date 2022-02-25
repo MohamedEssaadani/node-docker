5024abf8a454e28135295022f89ea7c2858ee877
Mohamed Es-saadani <essaadani80@gmail.com>
2022-02-25 21:36:18 +0100

    Dockerizing front end

--- films-app-frontend/src/App.js
+++ films-app-frontend/src/App.js
@@ -1,22 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/")
+      .then((res) => res.json())
+      .then((data) => setMovies(data));
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Movies List</h1>
+        {movies &&
+          movies.map((movie) => <div key={movie.id}>{movie.title}</div>)}
       </header>
     </div>
   );
